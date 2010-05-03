ec01afef760a509780c4c01ed3ccbaf8b44e8966
Mark S. Weiss <marksweiss@yahoo.com>
2010-05-03 19:16:54 -0400

    Modified find_items() to handle single-value and list elements, and match list elements on single value or multi-value args passed in.

--- organize-m_test.py
+++ organize-m_test.py
@@ -6,38 +6,13 @@ DATA_FILE = "orgm.dat"
 TEST_DATA_FILE = "orgm_test.dat"
 
 
-class Element(object):
-    # Don't use this with #Item.find_item()
-    ROOT = 'item'   
-    # Use these with #Item.find_item()
-    TITLE = 'title'
-    AREA = 'area'
-    PROJECT = 'project'
-    TAGS = 'tags'
-    ACTIONS = 'actions'
-    DUE_DATE = 'due_date'
-    NOTE = 'note'
-    
-    # Child lists of parent list come back as individual ordered dicts 
-    #  in a list, one dict for each child name/value pair (child list).  
-    #  So to get the one we care about "generically" we need an enum here 
-    #  for the index position of the dict that has the key (Element.X) 
-    #  passed as the element arg to this function            
-    INDEXES = {TITLE : 0, AREA : 1, PROJECT : 2, TAGS : 3, 
-               ACTIONS : 4, DUE_DATE : 5, NOTE : 6}
-                  
-    @staticmethod
-    def index(element):
-        return Element.INDEXES[element]
-
-
 # Possibly overdesign, but we model each type of YAML element we can have in an
 #  Item entry with a class, which handles str(), validation and repr() TODO
 # So Item can add new Elements just by choosing on of these Element types
 class RootElement(object):
     # Root has a label but no value
     def __init__(self, key):
-        if not key: key = Element.ROOT
+        if not key: key = Item.Element.ROOT
         self.key = key
     
     def __str__(self):
@@ -92,22 +67,47 @@ class ChildMultilineTextElement(object):
 #  so can be converted back to str() by PyYaml  
 class Item(object):
 
+    class Element(object):
+        # Don't use this with #Item.find_item()
+        ROOT = 'item'   
+        # Use these with #Item.find_item()
+        TITLE = 'title'
+        AREA = 'area'
+        PROJECT = 'project'
+        TAGS = 'tags'
+        ACTIONS = 'actions'
+        DUE_DATE = 'due_date'
+        NOTE = 'note'
+
+        # Child lists of parent list come back as individual ordered dicts 
+        #  in a list, one dict for each child name/value pair (child list).  
+        #  So to get the one we care about "generically" we need an enum here 
+        #  for the index position of the dict that has the key (Element.X) 
+        #  passed as the element arg to this function            
+        ITEM_INDEXES = {TITLE : 0, AREA : 1, PROJECT : 2, TAGS : 3, 
+                        ACTIONS : 4, DUE_DATE : 5, NOTE : 6}
+
+        @staticmethod
+        def item_index(element):
+            return Item.Element.ITEM_INDEXES[element]
+
+
     def __init__(self, title, **kwelements):      
-        self._root = RootElement(Element.ROOT)
+        self._root = RootElement(Item.Element.ROOT)
         # TODO validate title is a non-zero-length string, throw otherwise
-        self._title = ChildTextElement(Element.TITLE, title)        
+        self._title = ChildTextElement(Item.Element.TITLE, title)        
         # Now handle all the optional args, using kwargs because we want the 
         #  flexibility to pass any of these, skipping any we don't care about
         #  so it's not the right use for just default args
         area, project, tags, actions, due_date, note = self.__load_elems(kwelements)          
         # Now load element properties, with vals or None returned from __load_elems()
-        self._area = ChildTextElement(Element.AREA, area)
-        self._project = ChildTextElement(Element.PROJECT, project)        
-        self._tags = ChildListElement(Element.TAGS, tags)
-        self._actions = ChildListElement(Element.ACTIONS, actions)
+        self._area = ChildTextElement(Item.Element.AREA, area)
+        self._project = ChildTextElement(Item.Element.PROJECT, project)        
+        self._tags = ChildListElement(Item.Element.TAGS, tags)
+        self._actions = ChildListElement(Item.Element.ACTIONS, actions)
         # TODO real date type and validation - create a new Element type
-        self._due_date = ChildTextElement(Element.DUE_DATE, due_date)
-        self._note = ChildMultilineTextElement(Element.NOTE, note)
+        self._due_date = ChildTextElement(Item.Element.DUE_DATE, due_date)
+        self._note = ChildMultilineTextElement(Item.Element.NOTE, note)
     
     # Props so string values for each Element in the Item are directly available
     @property
@@ -163,18 +163,18 @@ class Item(object):
       due_date = None
       note = None        
       elems = kwelements.keys()        
-      if Element.AREA in elems:
-          area = kwelements[Element.AREA]
-      if Element.PROJECT in elems:
-          project = kwelements[Element.PROJECT]
-      if Element.TAGS in elems: 
-          tags = kwelements[Element.TAGS]
-      if Element.ACTIONS in elems: 
-          actions = kwelements[Element.TAGS]
-      if Element.DUE_DATE in elems: 
-          due_date = kwelements[Element.DUE_DATE]
-      if Element.NOTE in elems: 
-          note = kwelements[Element.NOTE]
+      if Item.Element.AREA in elems:
+          area = kwelements[Item.Element.AREA]
+      if Item.Element.PROJECT in elems:
+          project = kwelements[Item.Element.PROJECT]
+      if Item.Element.TAGS in elems: 
+          tags = kwelements[Item.Element.TAGS]
+      if Item.Element.ACTIONS in elems: 
+          actions = kwelements[Item.Element.TAGS]
+      if Item.Element.DUE_DATE in elems: 
+          due_date = kwelements[Item.Element.DUE_DATE]
+      if Item.Element.NOTE in elems: 
+          note = kwelements[Item.Element.NOTE]
       return area, project, tags, actions, due_date, note
 
 # Manages an Organizem TODO data file, binding to a data file by name
@@ -194,27 +194,43 @@ class Organizem:
         with open(self.data_file, 'a') as f:         
             f.write(str(item))
     
-    # NOTE: Finds *first* matching item, trying to match the element, value passed
+    # NOTE: Finds all matching items, trying to match the element, value passed
     #  in, e.g. TITLE and a title value, PROJECT and a project value, etc.
     # NOTE: element must be one of the "enums" in class Element (other than ROOT)
     # NOTE: Returns the entire item as a Python object (dict/list combo)
-    def find_item(self, element, val):
+    def find_items(self, element, val):
         # TEMP DEBUG
         #with open(self.data_file, 'r') as f:         
         #    lines = f.readlines()
         #    for line in lines:
-        #      print line                
+        #      print line
+        ret = []                
         self._load()
         for item in self.data:
-            item_data = item[Element.ROOT]
+            item_data = item[Item.Element.ROOT]
             # Child lists of parent list come back as individual ordered dicts 
             #  in a list, one dict for each child name/value pair (child list).  
             #  So to get the one we care about "generically" we need an enum here 
             #  for the index position of the dict that has the key (Element.X) 
-            #  passed as the element arg to this function            
-            if item_data[Element.index(element)][element] == val:
-                return item_data
-        return None
+            #  passed as the element arg to this function
+            # Get value for the element of interest, handle cases of string and list            
+            comp_val = item_data[Item.Element.item_index(element)][element]
+            if isinstance(comp_val, str) and comp_val == val:
+                ret.append(item_data)
+            elif isinstance(comp_val, list):
+                # If the value coming back is a list, it's tags or actions
+                # Support letting the caller pass a single value (e.g. one tag)
+                #  or a list of values (i.e. match on any of the values passed in)
+                # So handle the case here to test for whether val is scalar or list
+                if isinstance(val, list):
+                    for v in val:
+                        if v in comp_val:
+                            ret.append(item_data)
+                elif isinstance(val, str):
+                    if val in comp_val:
+                        ret.append(item_data)
+        # If no matches this returns empty list, which evaluates to "False"
+        return ret
 
 
 # TDD all the way ... :-)  
@@ -247,25 +263,45 @@ class OrganizemTestCase(unittest.TestCase):
         item = Item(title)
         orgm = Organizem(TEST_DATA_FILE)
         orgm.add_item(item)                
-        self.assertTrue(orgm.find_item(Element.TITLE, title))
+        self.assertTrue(orgm.find_items(Item.Element.TITLE, title))
 
-    def test_add_item__find_item_by_area(self):
+    def test_add_item__find_items_by_area(self):
         self._init_test_data_file()
         title = "title"
         area = "my area"
         item = Item(title, area=area)
         orgm = Organizem(TEST_DATA_FILE)
         orgm.add_item(item)                
-        self.assertTrue(orgm.find_item(Element.AREA, area))
+        self.assertTrue(orgm.find_items(Item.Element.AREA, area))
 
-    def test_add_item__find_item_by_project(self):
+    def test_add_item__find_items_by_project(self):
         self._init_test_data_file()
         title = "title"
         project = "my project"
         item = Item(title, project=project)
         orgm = Organizem(TEST_DATA_FILE)
         orgm.add_item(item)                
-        self.assertTrue(orgm.find_item(Element.PROJECT, project))
+        self.assertTrue(orgm.find_items(Item.Element.PROJECT, project))
+
+    def test_add_item__find_items_by_tags(self):
+        self._init_test_data_file()
+        title = "title"
+        # Test case of single-value passed to find_items() for a 
+        #  element that is stored in item as a list (tags)
+        tag1 = 'tag 1'
+        tags1 = [tag1]
+        item1 = Item(title, tags=tags1)
+        orgm = Organizem(TEST_DATA_FILE)
+        orgm.add_item(item1)
+        self.assertTrue(orgm.find_items(Item.Element.TAGS, tag1))
+        # Test case of multi-value list passed to find_items() for a 
+        #  element that is stored in item as a list (tags)
+        tag2 = 'tag 2'
+        tags2 = [tag1, tag2]
+        item2 = Item(title, tags=tags2)
+        orgm.add_item(item2)
+        self.assertTrue(orgm.find_items(Item.Element.TAGS, tag2))
+        self.assertTrue(orgm.find_items(Item.Element.TAGS, tags2))
 
 
 if __name__ == '__main__':
