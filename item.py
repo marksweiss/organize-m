d772c2b86a904f5b3af39426865f38368612572e
Mark S. Weiss <marksweiss@yahoo.com>
2010-05-09 02:59:10 -0400

    Refactored with new Action and ActionArg constants to remove magic strings from orgm.py CLI class and organizem.py

--- item.py
+++ item.py
@@ -7,13 +7,13 @@ import yaml
 class RootElement(object):
     # Root has a label but no value
     def __init__(self, key):
-        if not key: key = Item.Element.ROOT
+        if not key: key = Elem.ROOT
         self.key = key
     
     def __str__(self):
         # Leading line break to always init a new Element block
         #  validly at the start of a new line
-        return "\n- %s:" % self.key
+        return "\n\n- %s:" % self.key
 
 # TODO - Validate val can validly convert to str if it's not None
 class ChildTextElement(object):
@@ -55,6 +55,31 @@ class ChildMultilineTextElement(object):
       return "\n".join(ret)
 
 
+class Elem(object):
+    # Don't use this with #Item.find_item()
+    ROOT = 'item'   
+    # Use these with #Item.find_item()
+    TITLE = 'title'
+    AREA = 'area'
+    PROJECT = 'project'
+    TAGS = 'tags'
+    ACTIONS = 'actions'
+    DUE_DATE = 'due_date'
+    NOTE = 'note'
+
+    # Child lists of parent list come back as individual ordered dicts 
+    #  in a list, one dict for each child name/value pair (child list).  
+    #  So to get the one we care about "generically" we need an enum here 
+    #  for the index position of the dict that has the key (Element.X) 
+    #  passed as the element arg to this function            
+    ITEM_INDEXES = {TITLE : 0, AREA : 1, PROJECT : 2, TAGS : 3, 
+                    ACTIONS : 4, DUE_DATE : 5, NOTE : 6}
+
+    @staticmethod
+    def index(element):
+        return Elem.ITEM_INDEXES[element]
+
+
 # A single Item in the TODO file, with a root 'item:' element and child
 #  elements for each of the TODO fields, title, project, area, tags, actions
 #  due_date and notes
@@ -66,48 +91,37 @@ class ChildMultilineTextElement(object):
 #  so can be converted back to str() by PyYaml  
 class Item(object):
 
-    class Element(object):
-        # Don't use this with #Item.find_item()
-        ROOT = 'item'   
-        # Use these with #Item.find_item()
-        TITLE = 'title'
-        AREA = 'area'
-        PROJECT = 'project'
-        TAGS = 'tags'
-        ACTIONS = 'actions'
-        DUE_DATE = 'due_date'
-        NOTE = 'note'
-
-        # Child lists of parent list come back as individual ordered dicts 
-        #  in a list, one dict for each child name/value pair (child list).  
-        #  So to get the one we care about "generically" we need an enum here 
-        #  for the index position of the dict that has the key (Element.X) 
-        #  passed as the element arg to this function            
-        ITEM_INDEXES = {TITLE : 0, AREA : 1, PROJECT : 2, TAGS : 3, 
-                        ACTIONS : 4, DUE_DATE : 5, NOTE : 6}
-
-        @staticmethod
-        def item_index(element):
-            return Item.Element.ITEM_INDEXES[element]
-
-
     def __init__(self, title, **kwelements):      
-        self._root = RootElement(Item.Element.ROOT)
+        self._root = RootElement(Elem.ROOT)
         # TODO validate title is a non-zero-length string, throw otherwise
-        self._title = ChildTextElement(Item.Element.TITLE, title)        
+        self._title = ChildTextElement(Elem.TITLE, title)        
         # Now handle all the optional args, using kwargs because we want the 
         #  flexibility to pass any of these, skipping any we don't care about
         #  so it's not the right use for just default args
         area, project, tags, actions, due_date, note = self.__load_elems(kwelements)          
         # Now load element properties, with vals or None returned from __load_elems()
-        self._area = ChildTextElement(Item.Element.AREA, area)
-        self._project = ChildTextElement(Item.Element.PROJECT, project)        
-        self._tags = ChildListElement(Item.Element.TAGS, tags)
-        self._actions = ChildListElement(Item.Element.ACTIONS, actions)
+        self._area = ChildTextElement(Elem.AREA, area)
+        self._project = ChildTextElement(Elem.PROJECT, project)        
+        self._tags = ChildListElement(Elem.TAGS, tags)
+        self._actions = ChildListElement(Elem.ACTIONS, actions)
         # TODO real date type and validation - create a new Element type
-        self._due_date = ChildTextElement(Item.Element.DUE_DATE, due_date)
-        self._note = ChildMultilineTextElement(Item.Element.NOTE, note)
-        
+        self._due_date = ChildTextElement(Elem.DUE_DATE, due_date)
+        self._note = ChildMultilineTextElement(Elem.NOTE, note)
+    
+    @staticmethod
+    def py_item_to_item(py_obj_item):
+        """Converts Item serialized to Python object form, dicts and lists, to YAML"""
+        root = RootElement(Elem.ROOT)
+        elems = py_obj_item[Elem.ROOT]
+        title = elems[Elem.index(Elem.TITLE)][Elem.TITLE]
+        area = elems[Elem.index(Elem.AREA)][Elem.AREA]
+        project = elems[Elem.index(Elem.PROJECT)][Elem.PROJECT]
+        tags = elems[Elem.index(Elem.TAGS)][Elem.TAGS]        
+        actions = elems[Elem.index(Elem.ACTIONS)][Elem.ACTIONS]
+        due_date = elems[Elem.index(Elem.DUE_DATE)][Elem.DUE_DATE]
+        note = elems[Elem.index(Elem.NOTE)][Elem.NOTE]
+        return Item(title, area=area, project=project, tags=tags, actions=actions, due_date=due_date, note=note)
+
     # Props so string values for each Element in the Item are directly available
     @property
     def title(self):
@@ -163,16 +177,16 @@ class Item(object):
       due_date = None
       note = None        
       elems = kwelements.keys()        
-      if Item.Element.AREA in elems:
-          area = kwelements[Item.Element.AREA]
-      if Item.Element.PROJECT in elems:
-          project = kwelements[Item.Element.PROJECT]
-      if Item.Element.TAGS in elems: 
-          tags = kwelements[Item.Element.TAGS]
-      if Item.Element.ACTIONS in elems: 
-          actions = kwelements[Item.Element.ACTIONS]
-      if Item.Element.DUE_DATE in elems: 
-          due_date = kwelements[Item.Element.DUE_DATE]
-      if Item.Element.NOTE in elems: 
-          note = kwelements[Item.Element.NOTE]
+      if Elem.AREA in elems:
+          area = kwelements[Elem.AREA]
+      if Elem.PROJECT in elems:
+          project = kwelements[Elem.PROJECT]
+      if Elem.TAGS in elems: 
+          tags = kwelements[Elem.TAGS]
+      if Elem.ACTIONS in elems: 
+          actions = kwelements[Elem.ACTIONS]
+      if Elem.DUE_DATE in elems: 
+          due_date = kwelements[Elem.DUE_DATE]
+      if Elem.NOTE in elems: 
+          note = kwelements[Elem.NOTE]
       return area, project, tags, actions, due_date, note
